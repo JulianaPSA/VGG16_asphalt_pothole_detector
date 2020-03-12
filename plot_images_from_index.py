2ff7ac145efee8c2b6917404e5e47a586f834161
Juliana Peixoto <alencar.julianapeixoto@gmail.com>
2020-03-12 14:46:52 -0300

    show misclassificated images

--- plot_images_from_index.py
+++ plot_images_from_index.py
@@ -1,13 +1,25 @@
-import shutil, os
+import os
+import cv2
 
 base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'dataset')
 test_dir = os.path.join(base_dir, 'test')
+b_index = [5, 7, 8, 11, 14, 17, 19, 23, 34, 77, 82, 84, 86, 87, 93, 102]
+#####
 
 sub_dirs = [dir for dir in os.listdir(test_dir) if os.path.isdir(os.path.join(test_dir, dir))]
-print(sub_dirs)
-# images = []
-#
-# [print(f_name) for f_name in os.listdir(os.path.join(test_dir, sub_dirs[0])).sort() if os.path.isfile(os.path.join(test_dir, sub_dirs[0], f_name)) ]
-#
-#
-# b_index = [2,5,7,8,11,14,17,19,23,34,77,82,84,86,87,93,102]
+sub_dirs.sort()
+i = -1
+for path in sub_dirs:
+    path = os.path.join(test_dir, path)
+    files = os.listdir(path)
+    files.sort()
+    for file in files:
+        i = i + 1
+        if i not in b_index:
+            continue
+        file = os.path.join(path, file)
+        if os.path.isfile(file):
+            img = cv2.imread(file)
+            cv2.imshow('image', img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
